66436c0b4d7445e04041e4411902991ef77770e2
Atharva Sakpal <atharvasakpal14@gmail.com>
2025-01-24 01:30:29 +0530

    hogya bhai

--- clientSide/src/pages/Result.jsx
+++ clientSide/src/pages/Result.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/appContext'
+import { useNavigate } from 'react-router-dom'
 
 const Result = () => {
 
 
   const {resultImage, image} = useContext(AppContext)
+
+  const navigate = useNavigate();
 
   return (
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
@@ -47,7 +50,7 @@ const Result = () => {
             {/* -------------_BUTTONS---------- */}
 
               { resultImage &&  <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6'>
-              <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700'>Try another image</button>
+              <button onClick={()=>navigate('/')} className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700'>Try another image</button>
               <a href={resultImage} download className='px-8 py-2.5 text-white text-sm bg-gradient-to-r  from-violet-700 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700'>Download Image</a>
             </div>
             }
